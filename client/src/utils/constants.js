b180ae4e29b693a7f4922e4dfa906a12d6d0dd69
holyaustin <41966983+holyaustin@users.noreply.github.com>
2022-06-26 12:40:02 +0100

    a

--- client/src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractAddress = "0xA44CC65c8B88cAACfAECa1D41cb1acb994aa9e12";
-export const contractABI = abi.abi;
--- client/src/utils/shortenAddress.js
+++
@@ -1,1 +0,0 @@
-export const shortenAddress = (address) => `${address.slice(0, 10)}...${address.slice(address.length - 8)}`;
