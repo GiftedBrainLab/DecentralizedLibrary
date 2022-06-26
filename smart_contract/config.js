08d0215647b62989cc799eb0e8fbc0c7b2b3e85b
holyaustin <41966983+holyaustin@users.noreply.github.com>
2022-06-26 12:39:35 +0100

    a

--- smart_contract/config.js
+++
@@ -1,5 +0,0 @@
-
-  export const EbookAddress = "0x9566322df4FF3F72398C1309c5a4eb3EeE985b9E"
-  export const VideoBookAddress = "0x4CfD01CbF6FAFD34778D5396ed6F50C1C002132C"
-  export const ProfileAddress= "0xc642Ba47D5B94C2a5d3a2462E870bce7406DADa2"
-  
